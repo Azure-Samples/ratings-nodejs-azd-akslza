a9e318261d8e8ecaca862b2776eb81f63b09e880
Carmody Quinn <carmodyquinn@microsoft.com>
2023-06-17 17:43:34 -0500

    change prepovision shell

--- hooks/preprovision.sh
+++ hooks/preprovision.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+###   #!/bin/sh
 echo "Prior to source"
-source .azure/${AZURE_ENV_NAME}/.env
-echo "after source"
-if ! [ -z "${AZURE_INFRA_NAME}"]; then
-   echo "Error, AZURE_INFRA_NAME environment variable not found.  Exiting."
-   exit;
-fi
-azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
+# source .azure/${AZURE_ENV_NAME}/.env
+# echo "after source"
+# if ! [ -z "${AZURE_INFRA_NAME}"]; then
+#    echo "Error, AZURE_INFRA_NAME environment variable not found.  Exiting."
+#    exit;
+# fi
+# azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
 
 
 
