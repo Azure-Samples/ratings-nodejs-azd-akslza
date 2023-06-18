7e6d15bbc81a29f57bd487c05a27638038eff635
Carmody Quinn <carmodyquinn@microsoft.com>
2023-06-18 08:39:59 -0500

    Revert changes on preprovisioning hooks

--- hooks/preprovision.sh
+++ hooks/preprovision.sh
@@ -1,12 +1,7 @@
-###   #!/bin/sh
-echo "Prior to source"
-# source .azure/${AZURE_ENV_NAME}/.env
-# echo "after source"
-# if ! [ -z "${AZURE_INFRA_NAME}"]; then
-#    echo "Error, AZURE_INFRA_NAME environment variable not found.  Exiting."
-#    exit;
-# fi
-# azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
+#!/bin/bash
+source .azure/${AZURE_ENV_NAME}/.env
+apt update && apt -y install jq
 
+AZURE_INFRA_NAME=$(cat .azure/dev/config.json |jq -c '.infra.parameters.name'|tr -d \")
 
-
+azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
