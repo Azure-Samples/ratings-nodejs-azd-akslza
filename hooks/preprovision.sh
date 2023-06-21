810b1f27b6cc3976126af2c950c3358d66a9c5f6
Carmody Quinn <carmodyquinn@microsoft.com>
2023-06-21 12:11:19 -0500

    change script

--- hooks/preprovision.sh
+++ hooks/preprovision.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-source .azure/${AZURE_ENV_NAME}/.env
+echo "hello world"
 
-AZURE_INFRA_NAME=$(cat .azure/dev/config.json |jq -c '.infra.parameters.name'|tr -d \")
+#source .azure/${AZURE_ENV_NAME}/.env
 
-azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
+# AZURE_INFRA_NAME=$(cat .azure/dev/config.json |jq -c '.infra.parameters.name'|tr -d \")
+
+# azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
