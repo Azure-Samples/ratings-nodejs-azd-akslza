b0552a1de82e80efe771ed82b150aec604547734
Carmody Quinn <carmodyquinn@microsoft.com>
2023-06-25 11:45:51 -0500

    update hooks

--- hooks/postprovision.sh
+++ hooks/postprovision.sh
@@ -4,6 +4,11 @@ SIGNED_IN_USER=$(az ad signed-in-user show --query id -o tsv)
 AKS_ID=$(az aks show --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_AKS_CLUSTER_NAME --query id -o tsv)
 AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN='b1ff04bb-8a4e-4dc4-8eb5-8693973ce19b'
 
+if [[ -z "${SIGNED_IN_USER}"]]; then
+   echo "Signed in user not found.  Skipping AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN role assignment."
+   exit
+fi
+
 az role assignment create --role ${AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN} \
    --assignee-object-id ${SIGNED_IN_USER} --assignee-principal-type User --scope ${AKS_ID}
 
--- hooks/preprovision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. .azure/${AZURE_ENV_NAME}/.env
-echo "hello world"
-
-# AZURE_INFRA_NAME=$(cat .azure/dev/config.json |jq -c '.infra.parameters.name'|tr -d \")
-
-# azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
