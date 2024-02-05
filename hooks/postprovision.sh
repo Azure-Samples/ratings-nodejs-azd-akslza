1e6fe4a936b46c5062144599780e7493060b074a
Carmody Quinn <carmody.quinn@microsoft.com>
2024-02-05 10:40:04 -0600

    update hooks

--- hooks/postprovision.sh
+++ hooks/postprovision.sh
@@ -8,16 +8,16 @@ fi
 #. .azure/cqaksazd02/.env
 echo "Starting postprovision.sh hook"
 AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN='b1ff04bb-8a4e-4dc4-8eb5-8693973ce19b'
-SIGNED_IN_USER=$(az ad signed-in-user show --query id -o tsv)
+AZURE_SIGNED_IN_USER=$(az ad signed-in-user show --query id -o tsv)
 echo "Found AKS Resource ID ${AZURE_AKS_RESOURCE_ID}"
 
-if [[ -z "${SIGNED_IN_USER}" ]]; then
+if [[ -z "${AZURE_SIGNED_IN_USER}" ]]; then
    echo "Signed in user not found.  Skipping AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN role assignment."
    exit
 fi
 
 az role assignment create --role ${AZURE_KUBERNETES_SERVICE_RBAC_CLUSTER_ADMIN} \
-   --assignee-object-id ${SIGNED_IN_USER} --assignee-principal-type User --scope ${AZURE_AKS_RESOURCE_ID}
+   --assignee-object-id ${AZURE_SIGNED_IN_USER} --assignee-principal-type User --scope ${AZURE_AKS_RESOURCE_ID}
 
 sleep 60
 
--- hooks/preprovision.sh
+++ hooks/preprovision.sh
@@ -10,5 +10,11 @@ if [[ -z "${AZURE_INFRA_NAME}" ]]; then
    read -p "Please provide the infrastructure name seed:" AZURE_INFRA_NAME
    azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
 fi
+AZURE_SIGNED_IN_USER=$(az ad signed-in-user show --query id -o tsv)
 
+if [[ -z "${AZURE_SIGNED_IN_USER}" ]]; then
+   echo "Signed in user not found.  Permissions on AKS will fail."
+   exit
+fi
+azd env set AZURE_SIGNED_IN_USER ${AZURE_SIGNED_IN_USER}
 
