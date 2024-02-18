43892cfd46d1cc835b0a7b4366c6272ff6025209
Carmody Quinn <carmodyquinn@microsoft.com>
2024-02-18 17:32:52 -0600

    add kubelogin to gh action and init submodules in preprovision hook

--- hooks/preprovision.sh
+++ hooks/preprovision.sh
@@ -6,6 +6,11 @@ if [[ ! -z "$CI" ]]; then
    exit
 fi
 
+echo "Initializing git submodules"
+git submodule init 
+echo "Updating git submodules"
+git submodule update
+
 if [[ -z "${AZURE_INFRA_NAME}" ]]; then
    read -p "Please provide the infrastructure name seed:" AZURE_INFRA_NAME
    azd env set AZURE_INFRA_NAME ${AZURE_INFRA_NAME}
